33cef105fe845648da57066cb824866147bf1973
Rafkat <rafkat-@mail.ru>
2020-05-18 23:24:53 +0300

    ReadMe added

--- system/forms.py
+++ system/forms.py
@@ -1,4 +1,3 @@
-from django import forms
-from .models import Recruit, Sith, Planet, Answer, TestShadowArm
+
 
 
--- system/views.py
+++ system/views.py
@@ -1,6 +1,4 @@
 from django.forms import modelform_factory
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 
